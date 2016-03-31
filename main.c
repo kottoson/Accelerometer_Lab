d5418e642c4cf4731bf01093db9cdbf38693d870
imjustjoshua <Joshua+github@royhousehold.net>
2016-03-31 09:29:45 -0400

    Filled main.c with some LED code for the sake of building. Will be adding .h files next.

--- main.c
+++ main.c
@@ -4,5 +4,30 @@
  *  Created on: Mar 31, 2016
  *      Author: student
  */
+#include <msp430.h>
+#include "LED.h"
 
-//This is the main.c Joshua made to test Git in CCS
+// Function prototypes
+void ConfigureClockModule(void);
+void TimeDelay(unsigned int delay, unsigned int resolution);
+void main(void)
+{
+	// Stop the watchdog timer, and configure the clock module.
+	WDTCTL = WDTPW + WDTHOLD;
+	ConfigureClockModule();
+
+    // Initialize port pins associated with the LEDs, and then turn off LEDs.
+    InitializeLEDPortPins();
+    TURN_ON_LED1;
+    TURN_OFF_LED2;
+
+	// Infinite loop
+}
+
+void ConfigureClockModule(void)
+{
+    // Configure Digitally Controlled Oscillator (DCO) for 16 MHz using factory
+    // calibrations.
+	DCOCTL  = CALDCO_1MHZ;
+	BCSCTL1 = CALBC1_1MHZ;
+}
